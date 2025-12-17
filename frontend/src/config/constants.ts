5e574cc3d330f07762c1a2096c0264c15c4a6b35
Dolu13 <demol.ludovic@yahoo.fr>
2025-12-17 15:34:56 +0100

    test config

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,8 +1,5 @@
 // En développement, utilise le proxy Vite pour éviter les problèmes CORS
-const isDevelopment = import.meta.env.DEV;
-const API_BASE_URL = isDevelopment 
-  ? '/api/function' 
-  : 'https://openfaas.91.99.16.71.nip.io/function';
+const API_BASE_URL = 'https://openfaas.91.99.16.71.nip.io/function';
 
 export const API_ENDPOINTS = {
   GENERATE_PASSWORD: `${API_BASE_URL}/generate-password`,
